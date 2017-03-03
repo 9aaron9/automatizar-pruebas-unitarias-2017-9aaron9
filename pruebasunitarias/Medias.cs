bc351e8746d424cd9aeaa4f00a9311eef1f41d68
9aaron9 <anegrete@ucol.mx>
2017-03-03 10:03:32 -0600

    Problema 00

---
+++ pruebasunitarias/Medias.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace pruebasunitarias
+{
+    class Medias
+    {
+        /**
+         * calcula y regresa la media aritmetica
+         **/
+        public static double mediaAritmetica(params int[] vals) { double i=5.656; return i; }
+        /**
+         * calcula y regresa la raiz enesima = x^(1/n)
+         **/
+        private static double raizEnesima(double x, int n) { double i = 5.656; return i; }
+        /**
+         * usa raizEnesima para calcular y regresar la media geometrica
+         **/
+        public double mediaGeometrica(params int[] vals) { double i = 5.656; return i; }
+        /**
+         * este metodo no esta implementado
+         **/
+        public static double meidaArmonica(params int[] vals) { double i = 5.656; return i; }
+    }
+}
